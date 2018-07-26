9935fc0843ada9c21402c89d27356806243acbcc
Jakub Szymański <jakubmateusz@poczta.onet.pl>
2018-07-26 16:34:37 +0200

    Switch from python3 to python in shebang

--- src/core.py
+++ src/core.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """
 A Linux program to create bootable Windows USB stick from a real Windows DVD or an image
--- src/list_devices.py
+++ src/list_devices.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """
 This file is part of WoeUSB.
